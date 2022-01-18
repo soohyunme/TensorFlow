0d4e4200d595144cdd521083d48f0a3d3764b558
soohyunme <soohyun.dev@gmail.com>
2022-01-18 00:52:34 +0000

    Upload new file
    Tensor Board Projector and modified utils.py

--- Code/16_Tensor_Board/utils.py
+++ Code/16_Tensor_Board/utils.py
@@ -53,7 +53,6 @@ def image_grid(data, labels, class_names):
           plt.imshow(data[i])
 
     return figure
-<<<<<<< HEAD
 
 def get_confusion_matrix(y_labels, logits, class_names):
     preds = np.argmax(logits, axis=1)
@@ -92,5 +91,71 @@ def plot_confusion_matrix(cm, class_names):
     cm_image = plot_to_image(figure)
 
     return cm_image
-=======
->>>>>>> e003dbad343375ac3a5f9042a1574a39b2da60ef
+
+# Stolen from:
+# https://gist.github.com/AndrewBMartin/ab06f4708124ccb4cacc4b158c3cef12
+def create_sprite(data):
+    """
+    Tile images into sprite image.
+    Add any necessary padding
+    """
+
+    # For B&W or greyscale images
+    if len(data.shape) == 3:
+        data = np.tile(data[..., np.newaxis], (1, 1, 1, 3))
+
+    n = int(np.ceil(np.sqrt(data.shape[0])))
+    padding = ((0, n ** 2 - data.shape[0]), (0, 0), (0, 0), (0, 0))
+    data = np.pad(data, padding, mode="constant", constant_values=0)
+
+    # Tile images into sprite
+    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3, 4))
+    # print(data.shape) => (n, image_height, n, image_width, 3)
+
+    data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
+    # print(data.shape) => (n * image_height, n * image_width, 3)
+    return data
+
+def plot_to_projector(
+    x, feature_vector, y, class_names, log_dir = 'default_log_dir',
+    meta_file = 'metadata.tsv'
+):
+    assert x.ndim == 4 # (BATCH, H, W, C)
+
+    if os.path.isdir(log_dir):
+        shutil.rmtree(log_dir)
+    
+    # Create a new clean fresh folder
+    os.mkdir(log_dir)
+
+    SPRITES_FILE = os.path.join(log_dir, 'sprites.png')
+    sprite = create_sprite(x)
+    cv2.imwrite(SPRITES_FILE, sprite)
+
+    # Generate label names
+    labels = [class_names[y[i]] for i in range(int(y.shape[0]))]
+
+    with open(os.path.join(log_dir, meta_file), 'w') as f:
+        for label in labels:
+            f.write('{}\n'.format(label))
+
+
+    if feature_vector.ndim != 2:
+        print('NOTE : Feature vector is not of form (BATCH, FEATURES)'
+            ' reshaping to try and get it to this form!')
+        feature_vector = tf.reshape(
+            feature_vector, [feature_vector.shape[0], -1]
+        )
+
+        feature_vector = tf.Variable(feature_vector)
+        checkpoint = tf.train.Checkpoint(embedding = feature_vector)
+        checkpoint.save(os.path.join(log_dir, 'embeddings.ckpt'))
+
+        # Set up config
+        config = projector.ProjectorConfig()
+        embedding = config.embeddings.add()
+        embedding.tensor_name = 'embedding/.ATTRIBUTES/VARIABLE_VALUE'
+        embedding.metadata_path = meta_file
+        embedding.sprite.image_path = 'sprites.png'
+        embedding.sprite.single_image_dim.extend((x.shape[1], x.shape[2]))
+        projector.visualize_embeddings(log_dir, config)
